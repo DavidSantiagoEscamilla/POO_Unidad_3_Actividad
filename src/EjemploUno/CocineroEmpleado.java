fbc3b0da11ad862a84a497bd10fada56d74ca232
sr.davidsantiago@hotmail.com <sr.davidsantiago@hotmail.com>
2024-11-20 19:54:34 -0500

    Modificaciones a Clases

--- src/EjemploUno/CocineroEmpleado.java
+++ src/EjemploUno/CocineroEmpleado.java
@@ -1,5 +1,8 @@
 package EjemploUno;
 
-public class CocineroEmpleado {
-
+public class CocineroEmpleado implements Cocinero {
+    @Override
+    public void cocinar() {
+    System.out.println("El cocinero está preparando la comida.");
+    }
 }
